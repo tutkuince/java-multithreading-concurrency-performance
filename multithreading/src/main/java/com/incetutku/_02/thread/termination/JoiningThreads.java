598744d567b1e2f2233ae52236c1596de41934d0
Tutku Ince <tutku.ince@outlook.com>
2024-10-09 16:22:06 +0300

    Joining Threads

---
+++ multithreading/src/main/java/com/incetutku/_02/thread/termination/JoiningThreads.java
@@ -0,0 +1,68 @@
+package com.incetutku._02.thread.termination;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JoiningThreads {
+    public static void main(String[] args) throws InterruptedException {
+        List<Long> inputNumbers = Arrays.asList(0L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
+
+        List<FactorialThread> threads = new ArrayList<>();
+
+        inputNumbers.forEach(inputNumber -> threads.add(new FactorialThread(inputNumber)));
+
+        threads.forEach(thread -> {
+            thread.setDaemon(true);
+            thread.start();
+        });
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        for (int i = 0; i < threads.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if (factorialThread.isFinished()) {
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress");
+            }
+        }
+    }
+
+    public static class FactorialThread extends Thread {
+        private long inputNumber;
+        private BigInteger result = BigInteger.ZERO;
+        private boolean isFinished = false;
+
+        public FactorialThread(long inputNumber) {
+            this.inputNumber = inputNumber;
+        }
+
+        @Override
+        public void run() {
+            this.result = factorial(inputNumber);
+            this.isFinished = true;
+        }
+
+        public BigInteger factorial(long n) {
+            BigInteger tempResult = BigInteger.ONE;
+
+            for (long i = n; i > 0; i--) {
+                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
+            }
+
+            return tempResult;
+        }
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public BigInteger getResult() {
+            return result;
+        }
+    }
+}
