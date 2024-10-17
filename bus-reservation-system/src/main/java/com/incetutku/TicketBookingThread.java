e1a8f0b1eee1f2cb70f7117d60334ce636ecd235
Tutku Ince <tutku.ince@outlook.com>
2024-10-17 22:38:15 +0300

    Thread

---
+++ bus-reservation-system/src/main/java/com/incetutku/TicketBookingThread.java
@@ -0,0 +1,20 @@
+package com.incetutku;
+
+import com.incetutku.model.TicketCounter;
+
+public class TicketBookingThread extends Thread {
+    private TicketCounter ticketCounter;
+    private String passengerName;
+    private int noOfSeatsToBook;
+
+    public TicketBookingThread(TicketCounter ticketCounter, String passengerName, int noOfSeatsToBook) {
+        this.ticketCounter = ticketCounter;
+        this.passengerName = passengerName;
+        this.noOfSeatsToBook = noOfSeatsToBook;
+    }
+
+    @Override
+    public void run() {
+        ticketCounter.bookTicket(passengerName, noOfSeatsToBook);
+    }
+}
