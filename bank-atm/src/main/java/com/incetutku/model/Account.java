36ca7a7b80d81b75b379608a6ebba04a38d265e2
Tutku Ince <tutku.ince@outlook.com>
2024-10-17 22:25:37 +0300

    Account

---
+++ bank-atm/src/main/java/com/incetutku/model/Account.java
@@ -0,0 +1,13 @@
+package com.incetutku.model;
+
+public class Account {
+    private int balance = 6000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
