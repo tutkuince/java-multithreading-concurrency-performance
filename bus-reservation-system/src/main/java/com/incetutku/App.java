463f8a5828aa5232ff6f987ab1d05f0238c6ddfb
Tutku Ince <tutku.ince@outlook.com>
2024-10-17 22:38:25 +0300

    app

--- bus-reservation-system/src/main/java/com/incetutku/App.java
+++ bus-reservation-system/src/main/java/com/incetutku/App.java
@@ -1,13 +1,17 @@
 package com.incetutku;
 
+import com.incetutku.model.TicketCounter;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        TicketCounter ticketCounter = new TicketCounter();
+        TicketBookingThread t1 = new TicketBookingThread(ticketCounter, "John", 2);
+        TicketBookingThread t2 = new TicketBookingThread(ticketCounter, "Terry", 2);
+
+        t1.start();
+        t2.start();
     }
 }
