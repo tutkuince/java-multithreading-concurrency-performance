140a7f39418b40c160d2b197f5026e29979b24ff
Tutku Ince <tutku.ince@outlook.com>
2024-10-17 22:25:58 +0300

    App

--- bank-atm/src/main/java/com/incetutku/App.java
+++ bank-atm/src/main/java/com/incetutku/App.java
@@ -1,13 +1,22 @@
 package com.incetutku;
 
+import com.incetutku.model.Account;
+import com.incetutku.worker.AccountHolder;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        Account account = new Account();
+        AccountHolder accountHolder = new AccountHolder(account);
+        Thread thread1 = new Thread(accountHolder);
+        Thread thread2 = new Thread(accountHolder);
+        thread1.setName("Jack");
+        thread2.setName("Martin");
+
+        thread1.start();
+        thread2.start();
     }
 }
